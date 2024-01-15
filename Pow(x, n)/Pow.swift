58b2cb4a4ed10e173d8681cf921613c27060c33e
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2024-01-15 18:23:12 +0600

    adding Pow.swift

---
+++ Pow(x, n)/Pow.swift
@@ -0,0 +1,16 @@
+/*
+Description: Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Link: https://leetcode.com/problems/powx-n/description/
+*/
+
+// solution in Swift
+
+import Foundation
+
+class Solution {
+    func myPow(_ x: Double, _ n: Int) -> Double {
+      // pow() is part of Swift and requires no extra library to be imported
+        return pow(x, Double(n))
+    }
+}
