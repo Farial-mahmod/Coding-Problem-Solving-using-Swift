e51f63477b8280f5bd8df133f993bf0fb117a35e
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2024-07-18 03:35:10 +0600

    adding SumAndReverse.swift

---
+++ Sum of Number and Its Reverse/SumAndReverse.swift
@@ -0,0 +1,30 @@
+/*
+Description: Given a non-negative integer num, return true if num can be expressed as the sum of any non-negative integer and its reverse, or false otherwise.
+
+Example 1:
+
+Input: num = 443
+Output: true
+Explanation: 172 + 271 = 443 so we return true.
+
+Link: https://leetcode.com/problems/sum-of-number-and-its-reverse/description/
+*/
+
+// solution using Swift
+
+class Solution {
+    func sumOfNumberAndReverse(_ num: Int) -> Bool {
+        guard num != 0 else { return true }
+
+        for i in 0..<num {
+
+            var reversedI = String(i).reversed()
+
+            if i + Int(String(reversedI))! == num {
+                return true
+            }
+
+        }
+        return false
+    }
+}
